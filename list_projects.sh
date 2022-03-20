000ea29cb2656e49a727f776f08939a9205f54f6
Niccari <latieos93@gmail.com>
2022-03-20 20:52:00 +0900

    add: also list python version

--- list_projects.sh
+++ list_projects.sh
@@ -14,7 +14,7 @@ VIRTUALENVS_PATH=$(get_virtualenvs_path $0)
 PROJECT_BASENAME=".project"
 
 project_paths=$(find ${VIRTUALENVS_PATH} -name ${PROJECT_BASENAME} -type f)
-messages="PROJECT_PATH\tCODE_PATH\tUNLINKED\tVIRTUALENV_SIZE"
+messages="PROJECT_PATH\tCODE_PATH\tPYTHON_VERSION\tUNLINKED\tVIRTUALENV_SIZE"
 
 for project_path in ${project_paths[@]}; do
   code_path=$(cat $project_path)
@@ -25,8 +25,8 @@ for project_path in ${project_paths[@]}; do
   fi
   virtualenv_path=${project_path//\/${PROJECT_BASENAME}}
   virtualenv_size=$(du -hs $virtualenv_path | cut -f 1)
-
-  messages="$messages\n$project_path\t$code_path\t$unlinked\t$virtualenv_size"
+  python_version=$(${virtualenv_path}/bin/python --version | cut -d " " -f 2)
+  messages="$messages\n$project_path\t$code_path\t$python_version\t$unlinked\t$virtualenv_size"
 done
 
 echo -e $messages
